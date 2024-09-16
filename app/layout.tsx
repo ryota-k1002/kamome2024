e2ee1f232febda69c7a1e8ad9a1340a9f685d10c
ryota-k1002 <cckw733253@gmail.com>
2024-09-16 11:55:16 +0900

    update layout.tsx | added Analytics Tag again

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<Analytics /></html></body>
     </html>
   )
 }
